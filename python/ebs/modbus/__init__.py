c8339025b50be87776755efe4363acccdce44f67
Chintalagiri Shashank <shashank.chintalagiri@gmail.com>
2018-04-16 10:34:05 +0530

    Fixed version in python __init__

--- python/ebs/modbus/__init__.py
+++ python/ebs/modbus/__init__.py
@@ -5,7 +5,7 @@ __author__ = 'Chintalagiri Shashank <shashank.chintalagiri@gmail.com>'
 
 from pkg_resources import get_distribution, DistributionNotFound
 try:
-    __version__ = get_distribution(__name__).version
+    __version__ = get_distribution('ebs-modbus').version
 except DistributionNotFound:
     # package is not installed
     from setuptools_scm import get_version
